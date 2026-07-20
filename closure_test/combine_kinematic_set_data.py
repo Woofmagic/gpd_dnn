0d5e34c9839c7d93aafe7ef58b778ab262390e06
MidnightExcursion <dima.watkins@gmail.com>
2026-07-20 17:27:31 -0400

    updated hpc script: now allowing for ht to be fit

--- closure_test/combine_kinematic_set_data.py
+++ closure_test/combine_kinematic_set_data.py
@@ -1,40 +1,44 @@
-##########################################
+####################################################################################################
 # FILE INFORMATION:
 # Purpose: generates a .csv file containing
 # pseudodata for training DNN models down
 # the line.
 # Created: 20260308
-# Last changed: 20260401
-##########################################
+# Last changed: 20260720
+####################################################################################################
 
 print("[INFO]: Script began running!")
 
-##########################################
+####################################################################################################
 # Importing Python Libraries
-##########################################
+####################################################################################################
 
-import pandas as pd
+import yaml
 import glob
 import os
 
+import pandas as pd
+
 print("[INFO]: Libraries imported!")
 
-##########################################
-# [IMPORTANT]: Static quantities parametrizing
-# the program. Change these if you need!
-##########################################
+####################################################################################################
+# [IMPORTANT]: Static quantities parametrizing the program. Change these if you need!
+####################################################################################################
 
 # verify this is what you want
 SCRATCH_PATH = 'placeholder!'
 
-VERSION_NUMBER = 1
-MINOR_NUMBER = 1
-MAJOR_MINOR_NUMBER = f"{VERSION_NUMBER}_{MINOR_NUMBER}"
+with open("config.yml", "r") as file:
+    config = yaml.safe_load(file)
+
+MAJOR_NUMBER = config["versioning"]["major"]
+MINOR_NUMBER = config["versioning"]["minor"]
+MAJOR_MINOR_NUMBER = f"{MAJOR_NUMBER}_{MINOR_NUMBER}"
 
 print(f"[INFO]: We are saving figures and data with the following appendage: {MAJOR_MINOR_NUMBER}")
 
 file_pattern = os.path.join(
-    SCRATCH_PATH, 
+    SCRATCH_PATH,
     f"version_{MAJOR_MINOR_NUMBER}", 
     "kinematic_set_*", 
     "data", 
@@ -58,5 +62,9 @@ total_pseudodata_file = os.path.join(
 combined_df.to_csv(total_pseudodata_file, index = False)
 
 print(f"[INFO]: Combined {len(kinematic_set_pseudodata_list)} files into {total_pseudodata_file}")
+
+####################################################################################################
+# end the script
+####################################################################################################
 
 print("[INFO]: End of script reached!")
