8e8a49ae3cddbc404fea5fb122854e3993cd0d15
MidnightExcursion <dima.watkins@gmail.com>
2026-07-22 16:46:59 -0400

    MAJOR: now includes CFF h-tilde analysis

--- closure_test/replica_average.py
+++ closure_test/replica_average.py
@@ -1,18 +1,20 @@
-##########################################
+#################################################################################
 # FILE INFORMATION:
 # Purpose: averages over all the replicas
 # Created: 20260107
-# Last changed: 20260407
-##########################################
+# Last changed: 20260722
+#################################################################################
 
 print("[INFO]: Script began running!")
 
-##########################################
+####################################################################################################
 # Importing Python Libraries
-##########################################
+####################################################################################################
 
+import yaml
 import sys
 import glob
+import gc
 
 import pandas as pd
 import numpy as np
@@ -38,16 +40,19 @@ from simultaneous_fit_dnn_config import bkm10_bsa
 
 print("[INFO]: Libraries imported!")
 
-##########################################
-# [IMPORTANT]: Static quantities parametrizing
-# the program. Change these if you need!
-##########################################
+#################################################################################
+# [IMPORTANT]: Static quantities parametrizing the program. Change these if you need!
+#################################################################################
 
-SCRATCH_PATH = 'placeholder'
+# verify this is what you want
+SCRATCH_PATH = 'placeholder!'
 
-VERSION_NUMBER = 1
-MINOR_NUMBER = 1
-MAJOR_MINOR_NUMBER = f"{VERSION_NUMBER}_{MINOR_NUMBER}"
+with open("config.yml", "r") as file:
+    config = yaml.safe_load(file)
+
+MAJOR_NUMBER = config["versioning"]["major"]
+MINOR_NUMBER = config["versioning"]["minor"]
+MAJOR_MINOR_NUMBER = f"{MAJOR_NUMBER}_{MINOR_NUMBER}"
 
 print(f"[INFO]: We are saving figures and data with the following appendage: {MAJOR_MINOR_NUMBER}")
 
@@ -56,30 +61,30 @@ TEST_TARGET_POLARIZATION = 0.0
 
 print(f"[INFO]: Detected lepton helicity to be: {'unpolarized' if TEST_LEPTON_HELICITY == 0.0 else 'polarized'}")
 
-##########################################
+#################################################################################
 # Tensorflow Configuration
-##########################################
+#################################################################################
 
 print(f"[INFO]: Physical devices available to TF are: {tf.config.list_physical_devices()}")
 print(f"[INFO]: Number of GPUs Available: {len(tf.config.list_physical_devices('GPU'))}")
 print(f"[INFO]: Number of CPUs Available: {len(tf.config.list_physical_devices('CPU'))}")
 print(f"[INFO]: Checking the GPU name: {tf.test.gpu_device_name()}")
 
-##########################################
+#################################################################################
 # Reading the Datafile:
-##########################################
+#################################################################################
 
 slurm_array_parameter = int(sys.argv[1])
 
 try:
     with open(
-        f"./pseudodata_slurm_logs/version_{MAJOR_MINOR_NUMBER}/valid_kinematic_sets_v{MAJOR_MINOR_NUMBER}.txt", 
+        f"./slurm_logs/version_{MAJOR_MINOR_NUMBER}/valid_kinematic_sets_v{MAJOR_MINOR_NUMBER}.txt", 
         "r",
         encoding = "utf-8") as good_kinematic_sets_file:
         # This creates a list like [2, 9, 15, ...]
         valid_kinematic_sets = [ int(line.strip()) for line in good_kinematic_sets_file if line.strip() ]
 except FileNotFoundError:
-    print(f"[ERROR]: Could not find pseudodata_slurm_logs/version_{MAJOR_MINOR_NUMBER}!")
+    print(f"[ERROR]: Could not find slurm_logs/version_{MAJOR_MINOR_NUMBER}!")
     sys.exit(0)
     
 print(f"[INFO]: Good kinematic sets were: {valid_kinematic_sets}")
@@ -92,9 +97,9 @@ except IndexError:
     print(f"[ERROR]: Slurm Array ID {slurm_array_parameter} is out of bounds for the valid sets list.")
     sys.exit(0)
 
-##########################################
-# Making individual replica predictions
-##########################################
+#################################################################################
+# load the replicas
+#################################################################################
 
 # find replicas:
 replica_paths = sorted(
@@ -109,6 +114,14 @@ replica_data_paths = sorted(
         f"{SCRATCH_PATH}/version_{MAJOR_MINOR_NUMBER}/kinematic_set_{kinematic_set_number}/data/dnn_data_replica_*_v{MAJOR_MINOR_NUMBER}.csv"
         )
     )
+
+if len(replica_paths) == 0:
+    print("[WARN]: No replicas found for this kinematic setting. Exiting...")
+    sys.exit(0)
+
+#################################################################################
+# IMPORTANT: use only *one* replica datafile to just get the constant numbers!
+#################################################################################
 
 first_replica_df = pd.read_csv(replica_data_paths[0])
 
@@ -137,9 +150,9 @@ CFF_IMAG_HT_KM15 = first_replica_df["Im[Ht]"].values
 CFF_REAL_ET_KM15 = first_replica_df["Re[Et]"].values
 CFF_IMAG_ET_KM15 = first_replica_df["Im[Et]"].values
 
-if len(replica_paths) == 0:
-    print("[WARN]: No replicas found for this kinematic setting. Exiting...")
-    sys.exit(0)
+#################################################################################
+# actually load the replicas:
+#################################################################################
 
 replicas = [tf.keras.models.load_model(
     path,
@@ -147,6 +160,10 @@ replicas = [tf.keras.models.load_model(
     safe_mode = False) for path in replica_paths]
 
 print(f"[INFO]: Loaded {len(replicas)} replica models.")
+
+#################################################################################
+# make the actual predictions:
+#################################################################################
 
 all_predictions = []
 replicas_cross_predictions = []
@@ -156,7 +173,8 @@ for index, replica in enumerate(replicas):
     current_csv_path = replica_data_paths[index]
     replica_df = pd.read_csv(current_csv_path)
     x_data = replica_df[["t", "x_b", "q_squared", "phi"]].values.astype(np.float64)
-    predicted_outputs = replica.predict(x_data, verbose = 0) # predicting using x_data
+    # predicting using x_data
+    predicted_outputs = replica.predict(x_data, verbose = 0)
     all_predictions.append(predicted_outputs)
 
     del replica_df
@@ -164,17 +182,28 @@ for index, replica in enumerate(replicas):
     del predicted_outputs
 
 all_predictions = np.array(all_predictions)
+
+# initialize all CFF prediction arrays even though we might not use all..
 replicas_h_real = []
 replicas_h_imag = []
+replicas_ht_real = []
+replicas_ht_imag = []
+replicas_e_real = []
+replicas_e_imag = []
+replicas_et_real = []
+replicas_et_imag = []
 
 for index, _ in enumerate(all_predictions):
 
     cff_h_real = all_predictions[index][:, 0] # getting Re[H]
     cff_h_imag = all_predictions[index][:, 1] # getting Im[H]
-    t = all_predictions[index][:, 2] # getting t
-    xb = all_predictions[index][:, 3] # getting xb
-    q_squared = all_predictions[index][:, 4] # getting Q^{2}
-    phi = all_predictions[index][:, 5]
+    cff_ht_real = all_predictions[index][:, 2] # getting Re[Ht]
+    cff_ht_imag = all_predictions[index][:, 3] # getting Im[Ht]
+
+    t = all_predictions[index][:, 4] # getting t
+    xb = all_predictions[index][:, 5] # getting xb
+    q_squared = all_predictions[index][:, 6] # getting Q^{2}
+    phi = all_predictions[index][:, 7]
 
     fe = compute_fe(t)
     fg = compute_fg(fe)
@@ -195,15 +224,17 @@ for index, _ in enumerate(all_predictions):
     cross_section = bkm10_cross_section(
         TEST_LEPTON_HELICITY, TEST_TARGET_POLARIZATION,
         q_squared, xb, t, epsilon, y_lep, xi, k, f1, f2, ktilde, tprime, phi, p1, p2,
-        cff_h_real, CFF_REAL_HT_KM15, CFF_REAL_E_KM15, CFF_REAL_ET_KM15, cff_h_imag, CFF_IMAG_HT_KM15, CFF_IMAG_E_KM15, CFF_IMAG_ET_KM15)
+        cff_h_real, cff_ht_real, CFF_REAL_E_KM15, CFF_REAL_ET_KM15, cff_h_imag, cff_ht_imag, CFF_IMAG_E_KM15, CFF_IMAG_ET_KM15)
 
     predicted_bsa = bkm10_bsa(
         TEST_TARGET_POLARIZATION,
         q_squared, xb, t, epsilon, y_lep, xi, k, f1, f2, ktilde, tprime, phi, p1, p2,
-        cff_h_real, CFF_REAL_HT_KM15, CFF_REAL_E_KM15, CFF_REAL_ET_KM15, cff_h_imag, CFF_IMAG_HT_KM15, CFF_IMAG_E_KM15, CFF_IMAG_ET_KM15)
+        cff_h_real, cff_ht_real, CFF_REAL_E_KM15, CFF_REAL_ET_KM15, cff_h_imag, cff_ht_imag, CFF_IMAG_E_KM15, CFF_IMAG_ET_KM15)
 
     replicas_h_real.append(cff_h_real)
     replicas_h_imag.append(cff_h_imag)
+    replicas_ht_real.append(cff_ht_real)
+    replicas_ht_imag.append(cff_ht_imag)
 
     replicas_cross_predictions.append(cross_section)
     replicas_bsa_predictions.append(predicted_bsa)
@@ -213,7 +244,8 @@ replicas_bsa_predictions = np.array(replicas_bsa_predictions)
 
 def crunch_statistics(data):
 
-    stats = {
+    # huge dictionary of statistics
+    statistics_dictionary = {
         'mean': np.mean(data, axis = 0),
         'std': np.std(data, axis = 0),
         'median': np.median(data, axis = 0),
@@ -221,11 +253,12 @@ def crunch_statistics(data):
         'max': np.max(data, axis = 0)
     }
 
-    for p in range(10, 50, 10):
-        stats[f'p{p}'] = np.percentile(data, p, axis = 0)
-        stats[f'p{100 - p}'] = np.percentile(data, 100 - p, axis = 0)
+    # adding keys to the dictionary:
+    for percentile in range(10, 50, 10):
+        statistics_dictionary[f'p{percentile}'] = np.percentile(data, percentile, axis = 0)
+        statistics_dictionary[f'p{100 - percentile}'] = np.percentile(data, 100 - percentile, axis = 0)
 
-    return stats
+    return statistics_dictionary
 
 # observable statistics:
 xs_stats = crunch_statistics(replicas_cross_predictions)
@@ -268,14 +301,25 @@ replica_statistics_dataframe.to_csv(
 # CFF statistics:
 cff_h_real_pred_per_replica = np.mean(all_predictions[:, :, 0], axis = 1)
 cff_h_imag_pred_per_replica = np.mean(all_predictions[:, :, 1], axis = 1)
+cff_ht_real_pred_per_replica = np.mean(all_predictions[:, :, 2], axis = 1)
+cff_ht_imag_pred_per_replica = np.mean(all_predictions[:, :, 3], axis = 1)
 
 replica_cffs_dataframe = pd.DataFrame({
     'ReH_pred': cff_h_real_pred_per_replica,
-    'ImH_pred': cff_h_imag_pred_per_replica
+    'ImH_pred': cff_h_imag_pred_per_replica,
+    'ReHt_pred': cff_ht_real_pred_per_replica,
+    'ImHt_pred': cff_ht_imag_pred_per_replica
 })
 
 replica_cffs_dataframe.to_csv(
     f"{SCRATCH_PATH}/version_{MAJOR_MINOR_NUMBER}/kinematic_set_{kinematic_set_number}/data/cff_replica_average_preds_v{MAJOR_MINOR_NUMBER}.csv", 
     index = False)
+
+tf.keras.backend.clear_session()
+gc.collect()
+
+####################################################################################################
+# end the script
+####################################################################################################
 
 print("[INFO]: End of script reached!")
